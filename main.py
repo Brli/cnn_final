980cdbc18c41b0b19d9b72b050695f41c55c09d0
BrLi <brli@chakralinux.org>
2019-12-31 01:56:53 +0800

    pass object directly from main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from tensorflow.keras import optimizers
+
 from libs import configure_gpu
 from network.tensor import inception_v3
 
 configure_gpu
-inception_v3.training(8, 0.0001, 'Adam', 'categorical_crossentropy')
+inception_v3.training(8, 0.0001, optimizers.Adam, 'categorical_crossentropy')
