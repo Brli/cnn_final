7412948b6894ca5a8c8f40d8eebe73d1d5e14f0f
BrLi <brli@chakralinux.org>
2019-12-30 22:45:52 +0800

    fix sample lib logic, import inception3

--- libs/pick_random_sample.py
+++ libs/pick_random_sample.py
@@ -4,7 +4,7 @@ from shutil import copy, rmtree
 import random
 
 
-def __init__(self, act: str, sample_size=50):
+def sampling(act: str, sample_size=50):
     # @act only accept "train" or "test"
     # @sample_size should be tunable out of scope, default to 50
     if act in ["train", "test"]:
@@ -20,19 +20,24 @@ def __init__(self, act: str, sample_size=50):
 
     # Pick random sample for training input
     # the project utilize directory structure is like:
-    #   <root_dir>/seg_train/<item class name>/XXX.{image mimetype}
+    #   <root_dir>/<act>/<item class name>/XXX.{image mimetype}
     # root_dir is 'data' or 'samples'
     # data/ stores the preset intel_image_classification images
     # samples/ stores the following sampled images
+    # act is train or test
+    # class name is the name of image set
     img_number = sample_size
+    target_dir = join("samples", act)
+    if isdir(target_dir):
+        # Clear the exist directory before we start sampling
+        rmtree(target_dir)
+
     for cls in classes:
-        target_dir = join("samples", act)
-        if isdir(target_dir):
-            # Clear the exist directory before we start sampling
-            rmtree(target_dir)
         # We pick the "img_number" amount of samples
         # NOTE: the number may be less, since we didn't deal with redundency
-        makedirs(target_dir)
+        makedirs(join(target_dir, cls))
         for x in range(0, img_number):
             samples = random.choice(listdir(join(src_dir, cls)))
             copy(join(src_dir, cls, samples), join(target_dir, cls, samples))
+
+    return target_dir
