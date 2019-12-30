8a16e87b015bd00fb5d40418a6367b27c3c7aaf6
BrLi <brli@chakralinux.org>
2019-12-30 23:52:54 +0800

    fix import typo

--- libs/pre_processing.py
+++ libs/pre_processing.py
@@ -1,7 +1,7 @@
-from tf.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
-def preprocessing(img_dir: str, pp_input, h, w, b):
+def preprocess(img_dir: str, pp_input, h, w, b):
     img_generator = ImageDataGenerator(preprocessing_function=pp_input)
     img_flow = img_generator.flow_from_directory(
         img_dir,
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from libs import configure_gpu
 from network.tensor import inception_v3
 
 configure_gpu
-inception_v3
+inception_v3.training(8, 0.0001, 'Adam', 'categorical_crossentropy')
